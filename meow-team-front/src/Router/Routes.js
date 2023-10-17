29e296b2efb5bfa86224ba071ef7f8b088b48349
maryam behroozi <m_behroozi@comp.iust.ac.ir>
2023-10-17 15:38:32 +0330

    product/v0.1/test_pmr

--- meow-team-front/src/Router/Routes.js
+++ meow-team-front/src/Router/Routes.js
@@ -1,8 +1,15 @@
 import LandingPage from '../Pages/LandingPage/LandingPage';
+import ProductPage from '../Pages/ProductPage/ProductPage';
 const routes = [
     {
       path: '/',
       component: LandingPage,
+      Private: false
+    }
+    ,
+    {
+      path: '/Product',
+      component: ProductPage,
       Private: false
     }
   ];
