e6ac148fa402a55c20c007ac7acdd03d4ee1dc40
Mafhoot <Mafhoot@gmail.com>
2023-12-14 18:40:56 +0330

    Create Base : Wallet

--- meow-team-front/src/Pages/Dashboard/Items.js
+++ meow-team-front/src/Pages/Dashboard/Items.js
@@ -2,7 +2,7 @@
 import User from '../../assets/User.svg'
 import Basket from '../../assets/Basket.svg'
 import Comment from '../../assets/comment.svg'
-import Wallet from '../../assets/Wallet.svg'
+import Wallet1 from '../../assets/Wallet.svg'
 import Route from '../../assets/Route.svg'
 import history from '../../assets/history.svg'
 import heart from '../../assets/heart.svg'
@@ -10,61 +10,62 @@ import Dashboard from '../../assets/Dashboard.svg'
 import {EditProfile} from '../../Components/EditProfile/EditProfile'
 import DefaultDashboard from '../../Components/DefaultDashboard/DefaultDashboard'
 import ShoppingCart from '../../Components/ShoppingCart/ShoppingCart'
+import { Wallet } from "../Wallet/Wallet"
 import { MyOrders } from '../../Components/MyOrders/MyOrders'
 export const items = [
-    {
-        name: 'داشبورد',
-        component: DefaultDashboard,
-        icon: Dashboard,
-        path: 'default',
-        item : 0
-    },
-    {
-        name: 'اطلاعات حساب کاربری',
-        component: EditProfile,
-        icon: User,
-        path: 'editprofile',
-        item : 1
-
-    },
-    {
-        name: 'سبد خرید',
-        component: ShoppingCart,
-        icon: Basket,
-        path: 'shoppingcart',
-        item : 2
-    },
-    {
-        name: 'سفارشات من',
-        component: MyOrders,
-        icon: history,
-        path: 'history',
-        item : 3
-    },
-    {
-        name: ' کیف پول من ',
-        component: EditProfile,
-        icon: Wallet,
-        path: 'wallet',
-        item : 4
-    },
-    {
-        name: 'علاقه‌مندی‌های من',
-        component: EditProfile,
-        icon: heart,
-        path: 'heart',
-        item : 5
-    },
-    // {
-    //     name: 'آدرس های من',
-    //     component: '',
-    //     icon: Route,
-    //     item : 6
-    // },
-    {
-        name: 'کامنت های من',
-        component: EditProfile,
-        icon: Comment,
-        path: 'comment',
-        item : 7
-    }]
+  {
+    name: "داشبورد",
+    component: DefaultDashboard,
+    icon: Dashboard,
+    path: "default",
+    item: 0,
+  },
+  {
+    name: "اطلاعات حساب کاربری",
+    component: EditProfile,
+    icon: User,
+    path: "editprofile",
+    item: 1,
+  },
+  {
+    name: "سبد خرید",
+    component: ShoppingCart,
+    icon: Basket,
+    path: "shoppingcart",
+    item: 2,
+  },
+  {
+    name: "سفارشات من",
+    component: MyOrders,
+    icon: history,
+    path: "history",
+    item: 3,
+  },
+  {
+    name: " کیف پول من ",
+    component: Wallet,
+    icon: Wallet1,
+    path: "wallet",
+    item: 4,
+  },
+  {
+    name: "علاقه‌مندی‌های من",
+    component: EditProfile,
+    icon: heart,
+    path: "heart",
+    item: 5,
+  },
+  // {
+  //     name: 'آدرس های من',
+  //     component: '',
+  //     icon: Route,
+  //     item : 6
+  // },
+  {
+    name: "کامنت های من",
+    component: EditProfile,
+    icon: Comment,
+    path: "comment",
+    item: 7,
+  },
+];
--- meow-team-front/src/Pages/Wallet/Wallet.jsx
+++ meow-team-front/src/Pages/Wallet/Wallet.jsx
@@ -1,9 +1,42 @@
 import React from "react";
+import styles from "./Wallet.module.scss";
 import { useState, useEffect } from "react";
 
+export function Wallet () {
 
-export const Wallet = () => {
+    const [walletAmount, setWalletAmount] = useState(0)
+
     return (
-        <></>
-    )
+      <div className={styles.WalletAll}>
+        <div className={styles.WalletTop}>
+          <div className={styles.WalletAmountSection}>
+            <div className={styles.WalletAmountText}>
+              <p>موجودی کیف پول شما</p>
+            </div>
+            <div className={styles.WalletAmount}>
+              <p>{walletAmount}</p>
+            </div>
+          </div>
+          <div className={styles.AddToWalletSection}>
+            <input
+              type="number"
+              name=""
+              id=""
+              className={styles.AddToWalletInput}
+            />
+            <button className={styles.AddToWalletButton}>افزایش موجودی</button>
+          </div>
+        </div>
+        <div className={styles.WalletBottom}>
+          <h2>سوابق تراکنش های شما</h2>
+          <div className={styles.WalletHistoryList}>
+            <div className={styles.WalletHistoryItem}>
+              <p className={styles.WalletHistoryItemAmount}></p>
+              <p className={styles.WalletHistoryItemCause}></p>
+              <p className={styles.WalletHistoryItemDate}></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
 }
